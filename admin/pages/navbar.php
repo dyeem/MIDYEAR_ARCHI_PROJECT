fe416000336454abef4a4a266514020328207753
Your Name <you@example.com>
2024-08-13 02:22:03 +0800

    aug 12 222am

---
+++ admin/pages/navbar.php
@@ -0,0 +1,48 @@
+<?php 
+session_start();
+?>
+<link rel="stylesheet" href="../../css/navbar.css">
+<div class="container-fluid">
+    <div class="row ">
+        <div class="col-2 first-col pb-5" >
+            <ul class="nav flex-column nav-pills">
+                <li class="nav-item mt-5 ">
+                    <div class=" text-light d-flex flex-column justify-content-center align-items-center">
+                        <h3 class="text-align-center mb-5">ADMIN SIDE</h3>
+                        <img src="" alt="test" class="nav-class">
+                        <p class="text-align-center"><?php ?>name of head placeholder</p>
+                        
+                        <small class="text-align-center fw-bold">Admin Head</small>
+                    </div>
+                </li>
+                <li class="nav-item mt-5">
+                    <p class="nav-link text-light navigate fs-3">Navigation</p>
+                </li>
+                <li class="nav-item select-container">
+                    <a class="nav-link fs-5" href="#">Admin Settings</a>
+                </li>
+                <li class="nav-item" id="adminNav">
+                    <a class="nav-link fs-5" href="#">Transaction Panel</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link fs-5" href="#">View all Users</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link fs-5" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+            </ul>
+        </div>
+        <div class="col-10">
+            <h4 class="text-light header">ADMIN SIDE</h4>
+        </div>
+    </div>
+</div>
